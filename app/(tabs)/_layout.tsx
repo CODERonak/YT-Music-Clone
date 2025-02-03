d06392cdb8ff4716cb13fe0253a402a712cc040d
r9988.2588 <r9988.2588@gmail.com>
2025-02-03 10:34:51 +0000

    added icons for tab bar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { Tabs } from "expo-router";
-// import "@/global.css";
+import Ionicons from '@expo/vector-icons/Ionicons';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+
+// icons
 
 const TabLayout = () => {
     return (
@@ -8,18 +11,51 @@ const TabLayout = () => {
             screenOptions={{
                 tabBarStyle: {
                     backgroundColor: '#1d1d1d',
-                    height: 50,
+                    height: 60,
                     borderTopWidth: 0,
                 },
 
                 tabBarActiveTintColor: 'white',
                 tabBarInactiveTintColor: 'white',
+                tabBarLabelStyle: {
+                    marginTop: 5
+                }
             }}
         >
-            <Tabs.Screen name="index" options={{ headerShown: false }} />
-            <Tabs.Screen name="Samples" options={{ headerShown: false }} />
-            <Tabs.Screen name="Explore" options={{ headerShown: false }} />
-            <Tabs.Screen name="Library" options={{ headerShown: false }} />
+            <Tabs.Screen
+                name="index"
+                options={{
+                    headerShown: false,
+                    tabBarIcon: ({ color, size, focused }) => <Ionicons name={focused ? 'home-sharp' : 'home-outline'} size={size} color={color} />,
+                    tabBarLabel: 'Home'
+                }}
+            />
+
+            <Tabs.Screen
+                name="Samples"
+                options={{
+                    headerShown: false,
+                    tabBarIcon: ({ color, size, focused }) => <Ionicons name={focused ? 'play-sharp' : 'play-outline'} size={size} color={color} />,
+                }}
+            />
+
+            <Tabs.Screen
+                name="Explore"
+                options={{
+                    headerShown: false,
+                    tabBarIcon: ({ color, size, focused }) => <Ionicons name={focused ? 'compass-sharp' : 'compass-outline'} size={size} color={color} />
+                }}
+            />
+
+            <Tabs.Screen
+                name="Library"
+                options={{
+                    headerShown: false,
+                    tabBarIcon: ({ color, size, focused }) => <MaterialCommunityIcons name={focused ? 'music-box-multiple' : 'music-box-multiple-outline'} size={size} color={color} />
+
+                }}
+            />
+            
         </Tabs>
     )
 }
