17a76388a094433d2a8c5809166b20b727f9bd16
r9988.2588 <r9988.2588@gmail.com>
2025-02-04 09:04:46 +0000

    added header component, few images and updated packages

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,15 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import Header from '@/components/Home/Header'
 
+// Home screen
 const index = () => {
     return (
         // background color
         <View className='bg-black flex-1'>
             {/* Main container */}
             <View>
-
+<Header/>
             </View>
         </View>
     )
