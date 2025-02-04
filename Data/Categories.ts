7c49ba36c313604af159f4dd15f943dae69bbf97
r9988.2588 <r9988.2588@gmail.com>
2025-02-04 12:06:41 +0000

    Added the categories btn

---
+++ Data/Categories.ts
@@ -0,0 +1,60 @@
+// Categories data
+
+const Categories = [
+    {
+        id: 1,
+        name: "Podcasts"
+    },
+    
+    {
+        id: 2,
+        name: "Romance"
+    },
+
+    {
+        id: 3,
+        name: "Feel good"
+    },
+
+    {
+        id: 4,
+        name: "Relax"
+    },
+
+    {
+        id: 5,
+        name: "Party"
+    },
+
+    {
+        id: 6,
+        name: "Energize"
+    },
+
+    {
+        id: 7,
+        name: "Workout"
+    },
+
+    {
+        id: 8,
+        name: "Commute"
+    },
+
+    {
+        id: 9,
+        name: "Sad"
+    },
+
+    {
+        id: 10,
+        name: "Focus"
+    },
+
+    {
+        id: 11,
+        name: "Sleep"
+    },
+]
+
+export default Categories;
--- app/components/Home/Header.tsx
+++ app/components/Home/Header.tsx
@@ -1,43 +1,77 @@
-import { View, Text, Image, Pressable } from 'react-native';
+import { View, Text, Image, Pressable, FlatList } from 'react-native';
 import React from 'react';
 import Ionicons from '@expo/vector-icons/Ionicons';
+import Categories from '@/Data/Categories';
 
 // Header component
 const Header = () => {
     return (
-        // Header container 
-        <View className='flex flex-row items-center justify-center'>
+        // Main Header container
+        <View>
+            {/* Header container  */}
+            <View className='flex flex-row items-center justify-center'>
 
-            {/* YT Music logo */}
-            <View className='ml-4 w-1/2'>
+                {/* YT Music logo */}
+                <View className='ml-4 w-1/2'>
 
-                {/* YT Music img */}
-                <Image
-                    source={require("@/assets/images/YTMusic.png")}
-                    className='w-28 h-12'
-                />
-            </View>
-
-            {/* Icons and Profile Button */}
-            <View className='w-1/2 flex flex-row justify-end '>
-                {/* notifications icon */}
-                <Pressable className='ml-4'>
-                    <Ionicons name="notifications-outline" size={24} color="white" />
-                </Pressable>
-
-                {/* search icon */}
-                <Pressable className='ml-4'>
-                    <Ionicons name="search-outline" size={24} color="white" />
-                </Pressable>
-
-                {/* profile pic */}
-                <Pressable className='ml-4'>
+                    {/* YT Music img */}
                     <Image
-                        source={require("@/assets/images/profile.jpg")}
-                        className='h-8 w-8 rounded-full mr-4'
+                        source={require("@/assets/images/YTMusic.png")}
+                        className='w-28 h-12'
                     />
-                </Pressable>
+                </View>
+
+                {/* Icons and Profile Button */}
+                <View className='w-1/2 flex flex-row justify-end'>
+                    {/* notifications icon */}
+                    <Pressable
+                        className='ml-4'
+                        android_ripple={{ color: 'rgba(0, 0, 0, 0.2)', foreground: true }}
+                    >
+                        <Ionicons name="notifications-outline" size={24} color="white" />
+                    </Pressable>
+
+                    {/* search icon */}
+                    <Pressable
+                        className='ml-4'
+                        android_ripple={{ color: 'rgba(0, 0, 0, 0.2)', foreground: true }}
+                    >
+                        <Ionicons name="search-outline" size={24} color="white" />
+                    </Pressable>
+
+                    {/* profile pic */}
+                    <Pressable
+                        className='ml-4'
+                        android_ripple={{ color: 'rgba(255, 255, 255, 0.2)', foreground: true }}
+                    >
+                        <Image
+                            source={require("@/assets/images/profile.jpg")}
+                            className='h-8 w-8 rounded-full mr-4'
+                        />
+                    </Pressable>
+                </View>
+
             </View>
+
+            {/* FlatList for rendering horizontal categories */}
+            <FlatList
+                data={Categories}
+                horizontal
+                showsHorizontalScrollIndicator={false}
+                renderItem={({ item }) => (
+                    // for adding it in a conatainer
+                    <View className='mt-6'>
+                        {/* for making it a button */}
+                        <Pressable
+                            className='bg-[#303642] px-4 py-2 mx-2 rounded-lg'
+                            android_ripple={{ color: 'rgba(255, 255, 255, 0.2)', foreground: true }}
+                        >
+                            <Text className='text-white text-xl font-bold'>{item.name}</Text>
+                        </Pressable>
+                    </View>
+                )}
+                keyExtractor={(item) => item.id.toString()}
+            />
         </View>
     );
 }
