24b3e985a08fbe7ad37853961304ac9aad9c1654
r9988.2588 <r9988.2588@gmail.com>
2025-02-05 09:20:57 +0000

    added 5 songs to the quick picks component

---
+++ Data/QuickPicksSongs.ts
@@ -0,0 +1,38 @@
+const QuickPickSongs = [
+    {
+        id: 1,
+        name: "Aaj Ki raat",
+        artist: "Sachin-Jigar, Bhattacharya, Divya",
+        image: "https://bucthfjkxohgkfhnervm.supabase.co/storage/v1/object/sign/YT%20Music%20Clone%20Bucktet/Images/Aaj%20ki%20raat.jpg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJZVCBNdXNpYyBDbG9uZSBCdWNrdGV0L0ltYWdlcy9BYWoga2kgcmFhdC5qcGciLCJpYXQiOjE3Mzg3NDQ0MDQsImV4cCI6MTc3MDI4MDQwNH0.rOWf_O3F0iZlQiDGrOTrp7BQRs3BXLEegh67pUfEA_o"
+    },
+
+    {
+        id: 2,
+        name: "Stranger Things (Extended)",
+        artist: "Kyle Dixon & Michael Stein",
+        image: "https://bucthfjkxohgkfhnervm.supabase.co/storage/v1/object/sign/YT%20Music%20Clone%20Bucktet/Images/Stranger%20Things.jpg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJZVCBNdXNpYyBDbG9uZSBCdWNrdGV0L0ltYWdlcy9TdHJhbmdlciBUaGluZ3MuanBnIiwiaWF0IjoxNzM4NzQ2NzEyLCJleHAiOjE3NzAyODI3MTJ9.cWwmZ5cp3Si9IOLXWTD5TZV0Iu45ztMYPIDO36yN7JM"
+    },
+
+    {
+        id: 3,
+        name: "History Is Now",
+        artist: "Natalie Holt",
+        image: "https://bucthfjkxohgkfhnervm.supabase.co/storage/v1/object/sign/YT%20Music%20Clone%20Bucktet/Images/Loki.jpg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJZVCBNdXNpYyBDbG9uZSBCdWNrdGV0L0ltYWdlcy9Mb2tpLmpwZyIsImlhdCI6MTczODc0Njc1MCwiZXhwIjoxNzcwMjgyNzUwfQ.axzSfwpe4HG5x4SxEHpV8Wny8IBaiuCfzjUVPEfuhEY"
+    },
+
+    {
+        id: 4,
+        name: "Arjan Vailly",
+        artist: "Bhupinder Babbal",
+        image: "https://bucthfjkxohgkfhnervm.supabase.co/storage/v1/object/sign/YT%20Music%20Clone%20Bucktet/Images/Arjan%20vailley.jpg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJZVCBNdXNpYyBDbG9uZSBCdWNrdGV0L0ltYWdlcy9BcmphbiB2YWlsbGV5LmpwZyIsImlhdCI6MTczODc0NjgzNSwiZXhwIjoxNzcwMjgyODM1fQ.ntDi-Rns8PvfVxPhcrLMaCT9g7Hkuwes37IbD8CGrxE"
+    },
+
+    {
+        id: 5,
+        name: "Bhool Bhulaiyaa 2 Title Track",
+        artist: "Neeraj Shridhar, Mellow D & Bob",
+        image: "https://bucthfjkxohgkfhnervm.supabase.co/storage/v1/object/sign/YT%20Music%20Clone%20Bucktet/Images/Bhool%20bhulaiyaa.jpg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJZVCBNdXNpYyBDbG9uZSBCdWNrdGV0L0ltYWdlcy9CaG9vbCBiaHVsYWl5YWEuanBnIiwiaWF0IjoxNzM4NzQ2OTE3LCJleHAiOjE3NzAyODI5MTd9.J0-c6JEjnBxVcD4OlH6FtWflrF2lGB_pcQ-iaVECzP4"
+    },
+]
+
+export default QuickPickSongs
--- app/components/Home/QuickPicks.tsx
+++ app/components/Home/QuickPicks.tsx
@@ -1,17 +1,48 @@
-import { View, Text, FlatList } from 'react-native'
+import { View, Text, FlatList, Image, Pressable } from 'react-native'
 import React from 'react'
+import QuickPickSongs from '@/Data/QuickPicksSongs'
 
 const QuickPicks = () => {
+
+    // for rendering the songs details
+    const renderItem = ({ item }) => {
+        return (
+            // added the extra view for avoiding the android ripple to overflow
+
+            <Pressable
+                android_ripple={{ color: 'rgba(255, 255, 255, 0.2)', foreground: true, }}
+                className='flex flex-row ml-6 items-center p-2 rounded-md w-96 overflow-hidden'>
+                {/* song image */}
+                <View>
+                    <Image
+                        source={{ uri: item.image }}
+                        className='h-16 w-16 rounded-md'
+                    />
+                </View>
+
+                {/* song name and artists */}
+                <View className='ml-4'>
+                    <Text className='text-white text-l font-bold'>{item.name}</Text>
+                    <Text className='text-gray-500 text-sm '>{item.artist}</Text>
+                </View>
+            </Pressable>
+
+        )
+    }
+
     return (
         // main container
         <View>
             {/* Heading */}
-            <View className='mt-12 ml-20'>
-                <Text className='text-white text-2xl font-bold'>Quick Picks</Text>
+            <View className='mt-10 ml-6'>
+                <Text className='text-white text-2xl font-bold mb-4'>Quick Picks</Text>
             </View>
 
             {/* songs */}
-            
+            <FlatList
+                data={QuickPickSongs}
+                renderItem={renderItem}
+            />
 
         </View>
     )
